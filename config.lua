9585657d03d079b39dc90a21bfa3faff4343f721
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2025-05-25 18:14:06 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,10 +3,10 @@ Config = {
 		Debug = false,
 		EventDebug = false,
 
-		Menu = "qb",				-- "qb", "ox"
-		Notify = "gta",				-- "qb", "ox", "gta"
-		ProgressBar = "gta",		-- "qb", "ox", "gta"
-		drawText = "gta",			-- "qb", "ox", "gta"
+		Menu = "ox",				-- "qb", "ox"
+		Notify = "ox",				-- "qb", "ox", "gta"
+		ProgressBar = "ox",		-- "qb", "ox", "gta"
+		drawText = "ox",			-- "qb", "ox", "gta"
 
 	},
 	Main = {
@@ -14,6 +14,6 @@ Config = {
 								-- This is intended to create a progressbar to show consuming but it was usually forces animations to end weirdly
 	},
 	Crafting = {
-		showItemBox = true		-- Display "use" item boxes on some inventories that need to be told to
+		showItemBox = false		-- Display "use" item boxes on some inventories that need to be told to
 	}
 }
